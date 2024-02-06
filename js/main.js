4a9a1ec3602d45165bae8d91c7b03c5abb13defa
drusjs <magnojoky962@gmail.com>
2024-02-06 21:33:13 +0300

    init anim

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.querySelector('.container-percent .question-circle').addEventListener('click', function() {
+    this.previousElementSibling.classList.remove('disable')
+    this.classList.add('disable')
+    setTimeout(()=>{
+        document.querySelector('.container-percent').classList.add('hide')
+        document.querySelector('.cow').classList.add('hide')
+        document.querySelector('.container-main').classList.remove('hide')
+    }, 2000)
+    setTimeout(()=>{
+        document.getElementById('first-modal').classList.remove('hide')
+    }, 3500)
+
+})
+
+document.querySelector('#first-modal .button-next').addEventListener('click', function() {
+    this.parentElement.classList.add('hide')
+    showAllButtons()
+})
+
+let buttonsDelay = 200
+let buttonsDelayStep = 200
+function showAllButtons() {    
+    document.querySelectorAll('.open-window').forEach(el=>{
+        setTimeout(()=>{el.classList.add('show')}, buttonsDelay)
+        buttonsDelay = +buttonsDelay + +buttonsDelayStep
+    })
+}
+
+let timerClose
+document.querySelectorAll('.open-window').forEach(el=>{
+    el.addEventListener('click', function() {
+        clearTimeout(timerClose)
+        document.getElementById('second-modal').classList.remove('hide')
+        timerClose = setTimeout(()=>{document.getElementById('second-modal').classList.add('hide')}, 10000)
+    })
+})
+document.querySelectorAll('.close-modal').forEach(el=>{
+    el.addEventListener('click', function() {
+        clearTimeout(timerClose)
+        this.closest('.modal').classList.add('hide')
+    })
+})
