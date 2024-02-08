aacbc85ad554a3463d1f6b91e55268bf1360875b
drusjs <magnojoky962@gmail.com>
2024-02-08 13:31:53 +0300

    update

--- js/main.js
+++ js/main.js
@@ -3,18 +3,32 @@ document.querySelector('.container-percent .question-circle').addEventListener('
     this.classList.add('disable')
     setTimeout(()=>{
         document.querySelector('.container-percent').classList.add('hide')
-        document.querySelector('.cow').classList.add('hide')
         document.querySelector('.container-main').classList.remove('hide')
     }, 2000)
     setTimeout(()=>{
-        document.getElementById('first-modal').classList.remove('hide')
-    }, 3500)
-
+        document.querySelector('.main-zoom-text').classList.remove('hide')
+    }, 2500)
 })
 
-document.querySelector('#first-modal .button-next').addEventListener('click', function() {
-    this.parentElement.classList.add('hide')
-    showAllButtons()
+document.querySelector('.question').addEventListener('click', function() {
+    this.classList.add('hide')
+    document.querySelector('.container-main').classList.add('bg')
+    document.querySelector('.cow').classList.add('hide')
+    document.querySelector('.main-zoom-text').classList.add('hide')
+    document.querySelector('.container-main').classList.remove('zoom')
+    setTimeout(()=>{
+        document.getElementById('first-modal').classList.remove('hide')
+    }, 2000)
+})
+
+document.querySelector('#first-modal').addEventListener('click', function() {
+    if (this.classList.contains('firstpage')) {
+        this.classList.remove('firstpage')
+    } else {
+        this.classList.add('hide')
+        showAllButtons()
+        inactivityTime()
+    }
 })
 
 let buttonsDelay = 200
@@ -24,6 +38,14 @@ function showAllButtons() {
         setTimeout(()=>{el.classList.add('show')}, buttonsDelay)
         buttonsDelay = +buttonsDelay + +buttonsDelayStep
     })
+    buttonsDelay = 200
+    buttonsDelayStep = 200
+    setTimeout(()=>{
+        document.querySelectorAll('.open-window').forEach(el=>{
+            setTimeout(()=>{el.classList.add('anim')}, buttonsDelay)
+            buttonsDelay = +buttonsDelay + +buttonsDelayStep
+        })
+    }, 3000)
 }
 
 let timerClose
@@ -43,14 +65,24 @@ document.querySelectorAll('.close-modal').forEach(el=>{
 })
 
 function resetPage() {
-    let elem = document.querySelector('.container-percent .question-circle')
+    let elem = document.querySelector('.container-percent .question-circle')    
+    inactivityTime(false)
     elem.previousElementSibling.classList.add('disable')
     elem.classList.remove('disable')
     document.querySelector('.container-percent').classList.remove('hide')
     document.querySelector('.cow').classList.remove('hide')
-    document.querySelector('.container-main').classList.add('hide')    
+    document.querySelector('.container-main').classList.add('hide')  
+    document.querySelector('.container-main').classList.remove('bg')   
+    document.querySelector('.container-main').classList.add('zoom')     
     document.getElementById('first-modal').classList.add('hide')
+    document.querySelector('.question').classList.remove('hide')
+    document.getElementById('first-modal').classList.add('firstpage')
+    document.querySelectorAll('.open-window').forEach(el=>{
+        el.classList.remove('show')
+        el.classList.remove('anim')
+    })
 }
+
 
 
 let icon = document.querySelector('#second-modal .icon')
@@ -63,3 +95,32 @@ function updateModal(num) {
     title.src = `images/modal/t${num}.svg`
     inner.src = `images/modal/bl${num}.svg`
 }
+
+let activeTimer = false
+
+function inactivityTime(set=true) {
+    let time
+    if (set) {
+        document.addEventListener('mousemove', resetTimer)
+        document.addEventListener('keypress', resetTimer)
+        activeTimer = true
+    } else {
+        document.removeEventListener('mousemove', resetTimer)
+        document.removeEventListener('keypress', resetTimer)
+        clearTimeout(time)
+        activeTimer = false
+    }
+
+  
+    function resetTimer() {
+      clearTimeout(time);
+      time = setTimeout(fn, 15000)
+    }
+  
+    function fn() {
+        if (activeTimer) {
+            resetPage()
+        }
+    }
+}
+  
